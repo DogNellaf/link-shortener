f3e5a13ad5f6bc414569206fbf89a3486f76d494
dognellaf <dans-host@ya.ru>
2024-10-25 11:39:12 +0300

    [Task 5/8/9/10/11] Созданы шаблоны для эндпоинтов

--- core/views.py
+++ core/views.py
@@ -1,4 +1,27 @@
+"""Содержит endpoints проекта"""
+from django.http import HttpResponseNotFound
 from django.shortcuts import render
 
 def index(request):
+    """Отображение view главной страницы"""
     return render(request, "index")
+
+def qr_generator(request):
+    """Отображение страницы QR-генератора"""
+    return render(request, "qr_generator")
+
+def history(request):
+    """Отображение страницы Мои ссылки и QR-коды"""
+    return render(request, "history")
+
+def price(request):
+    """Отображение страницы Тарифы"""
+    return HttpResponseNotFound()
+
+def account(request):
+    """Отображение страницы аккаунта пользователя"""
+    return render(request, "account")
+
+def privacy(request):
+    """Отображение страницы правил сервиса"""
+    return render(request, "privacy")
