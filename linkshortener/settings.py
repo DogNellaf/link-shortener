a24534416ce547b8111583c0f349b2a5148cd9b8
dognellaf <dans-host@ya.ru>
2024-10-30 17:10:02 +0300

    [Other] Dockerfile и postgres setting

--- linkshortener/settings.py
+++ linkshortener/settings.py
@@ -85,8 +85,12 @@ AUTH_USER_MODEL = 'custom_auth.CustomUser'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
