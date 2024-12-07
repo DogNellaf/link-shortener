5aa60f93271e53360f34c89a45039192e5c1bf25
DogNellaf <dans-host@ya.ru>
2024-12-07 10:31:29 +0300

    [Refactor] Предзагрузил шрифты в статику

--- core/static/js/base.js
+++ core/static/js/base.js
@@ -1,22 +1,156 @@
-backgroundColorPickerFrame.addEventListener('click', 
-    _ => backgroundColor.click()
-)
+const qrSizeInPixels = (window.innerWidth * 12.5) / 100;
+const logoSizeInPixels = (window.innerWidth * 3) / 100;
+let lastUrl = "";
+let lastLogoPath = "";
+let lastQrColor = "";
+let lastBackgroundColor = "";
+let isWithBackground = false;
 
-backgroundColor.addEventListener('input', 
-    _ => backgroundColorLabel.innerHTML = backgroundColor.value
-)
+function drawQR(canvas, url, logo_path, qr_color, background_color) {
+    lastUrl = url;
+    lastLogoPath = logo_path;
+    lastQrColor = qr_color;
+    lastBackgroundColor = background_color;
 
-qrColorPickerFrame.addEventListener('click', 
+    const uniqueIdSuffix = `-${Date.now()}`;
+
+    var qr = new QRCodeStyling({
+        type: "svg",
+        shape: "square",
+        width: qrSizeInPixels,
+        height: qrSizeInPixels,
+        data: lastUrl,
+        image: lastLogoPath,
+        qrOptions: {
+            typeNumber: "3",
+            mode: "Byte",
+            errorCorrectionLevel: "Q"
+        },
+        imageOptions: {
+            saveAsBlob: true,
+            hideBackgroundDots: true,
+            imageSize: 0.7,
+            margin: 5
+        },
+        dotsOptions: {
+            type: "extra-rounded",
+            color: lastQrColor,
+            roundSize: true,
+            gradient: null
+        },
+        backgroundOptions: {
+            color: isWithBackground? lastBackgroundColor : "transparent"
+        },
+        cornersSquareOptions: {
+            type: "extra-rounded",
+            color: lastQrColor,
+            gradient: null
+        },
+        svgOptions: {
+            id: `qr-svg${uniqueIdSuffix}`,
+        }
+    });
+
+    canvas.innerHTML = "";
+    qr.append(canvas);
+}
+
+function changeUrl(url) {
+    drawQR(
+        url,
+        lastLogoPath,
+        lastQrColor,
+        lastBackgroundColor
+    )
+}
+
+function changeQrColor(color) {
+    drawQR(
+        qrEditorCanvas,
+        lastUrl,
+        lastLogoPath,
+        color,
+        lastBackgroundColor
+    )
+}
+
+function changeBackgroundColor(color) {
+    drawQR(
+        qrEditorCanvas,
+        lastUrl,
+        lastLogoPath,
+        lastQrColor,
+        color
+    )
+}
+
+
+function changeLogoPath(path) {
+    drawQR(
+        qrEditorCanvas,
+        lastUrl,
+        path,
+        lastQrColor,
+        lastBackgroundColor
+    )
+}
+
+function changeBackgroundDraw() {
+    isWithBackground = !isWithBackground;
+    drawQR(
+        qrEditorCanvas,
+        lastUrl,
+        lastLogoPath,
+        lastQrColor,
+        lastBackgroundColor
+    )
+}
+
+qrColorPickerFrame.addEventListener('click',
     _ => qrColor.click()
-)
+);
 
 qrColor.addEventListener('input', 
-    _ => qrColorLabel.innerHTML = qrColor.value
-)
+    _ => {
+        let value = qrColor.value;
+        qrColorLabel.innerHTML = value;
+        changeQrColor(value);
+    }
+);
+
+backgroundDrawToogle.addEventListener('input', 
+    _ => changeBackgroundDraw()
+);
+
+backgroundColorPickerFrame.addEventListener('click', 
+    _ => backgroundColor.click()
+);
+
+backgroundColor.addEventListener('input', 
+    _ => {
+        let value = backgroundColor.value;
+        backgroundColorLabel.innerHTML = value;
+        changeBackgroundColor(value);
+    }
+);
 
 fileUploadButton.addEventListener('click', 
     _ => logo.click()
-)
+);
+
+logo.addEventListener('change', (e) => {
+    var selectedFile = e.target.files[0];
+    if (selectedFile != null) {
+        var reader = new FileReader();
+  
+        reader.onload = function(e) {
+            changeLogoPath(e.target.result);
+        };
+      
+        reader.readAsDataURL(selectedFile);
+    }
+});
+
 
 downloadSvgButton.addEventListener('click', function() {
     let svg = downloadQrImage.getElementsByClassName('segno')[0];
@@ -61,12 +195,12 @@ downloadPngButton.addEventListener('click', function() {
 });
 
 function showDownload(shortUrl) {
-    downloadQrImage.innerHTML = document.getElementById(shortUrl + "-qr").innerHTML;
+    // downloadQrImage.innerHTML = document.getElementById(shortUrl + "-qr").innerHTML;
     downloadQrBlock.removeAttribute("hidden");
 }
 
 function showQR(shortUrl) {
-    qrImageFrame.innerHTML = document.getElementById(shortUrl + "-qr").innerHTML;
+    // qrImageFrame.innerHTML = document.getElementById(shortUrl + "-qr").innerHTML;
     showQrFrame.removeAttribute("hidden");
 }
 
@@ -110,5 +244,7 @@ if (!localStorage.color) {
 }
 
 function closeFrame(frame) {
+    downloadQrImage.innerHTML = "";
+    qrEditorCanvas.innerHTML = "";
     frame.setAttribute("hidden", "hidden");
 }
